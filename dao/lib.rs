bfbfe677c7993843f21f1b4917c17f3e295f0d3c
Ganesh oli <ganesh@thaiofficefurniture.com>
2022-12-09 16:05:01 +0545

    dao wip

--- dao/lib.rs
+++ dao/lib.rs
@@ -26,8 +26,9 @@ mod dao {
     impl Dao {
         #[ink(constructor)]
         pub fn new(contribution_time: u64) -> Self {
+            let now = Self::env().block_timestamp();
             ink_lang::utils::initialize_contract(|_instance: &mut Dao|{
-                _instance.contribution_end = _instance.env().block_timestamp() + contribution_time;
+                _instance.contribution_end = now + contribution_time;
             })
         }
 
@@ -37,9 +38,42 @@ mod dao {
 
             let caller = self.env().caller();
             self.investors.insert(caller, &true);
-            self.shares.insert(caller, &(self.shares.get(caller).unwrap_or_default() + self.env().balance()));
-            self.total_shares += self.env().balance();
-            self.available_funds += self.env().balance();
+
+            let balance = self.env().balance();
+            let share = self.shares.get(caller).unwrap_or_default();
+            self.shares.insert(caller, &(share + balance));
+
+            self.total_shares += balance;
+            self.available_funds += balance;
         }
+
+        #[ink(message)]
+        pub fn redeem_share(&mut self, amount: Balance){
+            let caller = self.env().caller();
+            let now = self.env().block_timestamp();
+
+            assert!(now < self.contribution_end, "cannot contribute after contribution end");
+            assert!(self.available_funds >= amount, "not enough available funds");
+
+            let share = self.shares.get(caller).unwrap_or_default();
+
+            self.shares.insert(caller, &(share - amount));
+            self.available_funds -= amount;
+            
+            self.env().transfer(caller, amount).unwrap_or_default();
+
+        }
+
+        #[ink(message)]
+        pub fn transfer_share(&mut self, amount: Balance, to: AccountId) {
+            let caller = self.env().caller();
+            let share = self.shares.get(caller).unwrap_or_default();
+            assert!(share >= amount, "not enough share");
+
+            self.shares.insert(caller, &(share - amount));
+            self.shares.insert(to, &(share + amount));
+            self.investors.insert(to, &true);
+        }
+
     }
 }
